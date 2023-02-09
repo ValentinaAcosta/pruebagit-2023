f032ad8c4e59c13e5c8139bbf6dda07392e3d385
Valentina Acosta <valentina.acostalopez28@gmail.com>
2023-02-09 09:35:19 -0500

    se agrega la funcion sumar en el archivo app.js

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 let n1 = 7;
 let n2 = 3;
 /**/
+/**
+ * It subtracts the second number from the first number.
+ * @param num1 - The first number to subtract.
+ * @param num2 - number
+ * @returns The result of the subtraction.
+ */
 function restar(num1, num2) {
     let resultado = num1 - num2;
     console.log(`El resultado de la resta es ${resultado}`);
     return resultado;
 }
+function sumar(num1, num2) {
+    let resultado = num1 + num2;
+    console.log(`El resultado de la suma es ${resultado}`);
+    return resultado;
+}
 restar(n1, n2);
+sumar(n1, n2);
