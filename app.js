76e6a662a48b67fc47cd2214a8fbbe81b1aeacd2
Valentina Acosta <valentina.acostalopez28@gmail.com>
2023-02-09 09:16:47 -0500

    Se agrega app.js con funcion de resta y el archivo .gitignore

---
+++ app.js
@@ -0,0 +1,9 @@
+let n1 = 7;
+let n2 = 3;
+/**/
+function restar(num1, num2) {
+    let resultado = num1 - num2;
+    console.log(`El resultado de la resta es ${resultado}`);
+    return resultado;
+}
+restar(n1, n2);
