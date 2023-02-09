e5206b7aed01552ebfa5fe345d513b048df60b22
Valentina Acosta <valentina.acostalopez28@gmail.com>
2023-02-09 09:42:20 -0500

    Ajuste en app.js

--- app.js
+++ app.js
@@ -12,10 +12,17 @@ function restar(num1, num2) {
     console.log(`El resultado de la resta es ${resultado}`);
     return resultado;
 }
+/**
+ * Suma dos números y devuelve el resultado.
+ * @param num1 - El primer número a sumar.
+ * @param num2 - El segundo número a sumar.
+ * @returns El resultado de la suma de los dos números.
+ */
 function sumar(num1, num2) {
     let resultado = num1 + num2;
     console.log(`El resultado de la suma es ${resultado}`);
     return resultado;
 }
+
 restar(n1, n2);
 sumar(n1, n2);
